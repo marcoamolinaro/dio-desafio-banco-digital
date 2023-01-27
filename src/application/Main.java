eae8809c7153ffb353d0e56510c6408dea1f8f05
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-01-27 11:24:19 -0300

    Ajustes

---
+++ src/application/Main.java
@@ -0,0 +1,23 @@
+package application;
+import dominio.Cliente;
+import dominio.Conta;
+import dominio.ContaCorrente;
+import dominio.ContaPoupanca;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Marco");
+		
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+
+		cc.depositar(100);
+		cc.transferir(50, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
